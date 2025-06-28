3158b36ab45e52b4248e08f8f14862b1de33cd49
ivanya123 <aplesnin2010@yandex.ru>
2025-06-28 19:07:05 +0300

    Create logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -3,16 +3,15 @@ import logging
 
 def get_logger(name: str = "trading-bot", to_file: bool = False) -> logging.Logger:
     logger = logging.getLogger(name)
-    fmt = ("%(name)-15s | %(asctime)s | "
-           "%(module)-15s | line:%(lineno)4d | %(levelname)-8s | "
-           "%(message)s")
+    fmt = (
+        "%(name)-15s | %(asctime)s | "
+        "%(module)-15s | line:%(lineno)4d | %(levelname)-8s | "
+        "%(message)s"
+    )
     if not logger.handlers:
         logger.setLevel(logging.INFO)
         handler = logging.StreamHandler()
-        formatter = logging.Formatter(
-            fmt=fmt,
-            datefmt="%Y-%m-%d %H:%M:%S"
-        )
+        formatter = logging.Formatter(fmt=fmt, datefmt="%Y-%m-%d %H:%M:%S")
         handler.setFormatter(formatter)
         logger.addHandler(handler)
 
