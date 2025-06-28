f98c9f831d2189c99d8b4dd5f0e7ebeb3e52e6f8
ivanya123 <aplesnin2010@yandex.ru>
2025-06-28 20:45:56 +0300

    Create config

---
+++ config/config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_DIR / ".env"
+
+
+class Settings(BaseSettings):
+    db_url: str
+    jwt_secret: str
+    jwt_expire_minutes: int = 60
+    environment: str = "dev"
+
+    class Config:
+        env_file = ENV_PATH
+
+
+settings = Settings()
