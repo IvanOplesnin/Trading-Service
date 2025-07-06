9e0d57cc8bbf03ef9a3cb8f4983d3ea954c3ffa3
ivanya123 <aplesnin2010@yandex.ru>
2025-07-06 19:54:46 +0300

    Исправления Pydantic-схем, также исправления UserRepository

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,11 @@
-import models.pydantic_models.forward_refs
-
 from . import pydantic_models
 from .pydantic_models.trading_environment import *
 from .pydantic_models.user import *
 from .trading_environment import TradingEnvironment
 from .user import User
+
+ResponseUser.model_rebuild()
+ResponseEnvironment.model_rebuild()
 
 __all__ = [
     "User",
--- models/pydantic_models/forward_refs.py
+++
@@ -1,4 +0,0 @@
-from models import ReadEnvironment, ReadUser, ResponseEnvironment, ResponseUser
-
-ResponseUser.model_rebuild()
-ResponseEnvironment.model_rebuild()
